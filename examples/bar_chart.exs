c0bc4bee10375e1debe88a281c205411e6474111
Manuel Rubio <manuel@altenwald.com>
2026-08-20 01:34:28 +0200

    Update examples for multi-series bar chart data shape

--- examples/bar_chart.exs
+++ examples/bar_chart.exs
@@ -1,13 +1,29 @@
 data = [
-  %{label: "Jan", value: 42},
-  %{label: "Feb", value: 58},
-  %{label: "Mar", value: 33},
-  %{label: "Apr", value: 71},
-  %{label: "May", value: 65},
-  %{label: "Jun", value: 90}
+  %{
+    name: "Sales",
+    data: [
+      %{label: "Jan", value: 42},
+      %{label: "Feb", value: 58},
+      %{label: "Mar", value: 33},
+      %{label: "Apr", value: 71},
+      %{label: "May", value: 65},
+      %{label: "Jun", value: 90}
+    ]
+  },
+  %{
+    name: "Costs",
+    data: [
+      %{label: "Jan", value: 20},
+      %{label: "Feb", value: 25},
+      %{label: "Mar", value: 18},
+      %{label: "Apr", value: 30},
+      %{label: "May", value: 28},
+      %{label: "Jun", value: 35}
+    ]
+  }
 ]
 
-chart = Plotto.BarChart.new!(data, title: "Monthly Sales", name: "Sales", legend: :top_right)
+chart = Plotto.BarChart.new!(data, title: "Monthly Sales vs Costs", legend: :top_right)
 svg = Plotto.to_svg!(chart)
 png = Plotto.to_png!(chart)
 
--- examples/line_chart.exs
+++ examples/line_chart.exs
@@ -1,13 +1,18 @@
 data = [
-  %{label: "Jan", value: 42},
-  %{label: "Feb", value: 58},
-  %{label: "Mar", value: 33},
-  %{label: "Apr", value: 71},
-  %{label: "May", value: 65},
-  %{label: "Jun", value: 90}
+  %{
+    name: "Sales",
+    data: [
+      %{label: "Jan", value: 42},
+      %{label: "Feb", value: 58},
+      %{label: "Mar", value: 33},
+      %{label: "Apr", value: 71},
+      %{label: "May", value: 65},
+      %{label: "Jun", value: 90}
+    ]
+  }
 ]
 
-chart = Plotto.LineChart.new!(data, title: "Monthly Sales", name: "Sales", legend: :bottom_left)
+chart = Plotto.LineChart.new!(data, title: "Monthly Sales", legend: :bottom_left)
 svg = Plotto.to_svg!(chart)
 png = Plotto.to_png!(chart)
 
